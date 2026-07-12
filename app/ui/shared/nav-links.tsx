54b1ec359d797d1be40ae8fd93a2e80b31bc5a99
Hendrik <hrottgers@protonmail.com>
2026-07-12 11:55:54 +0200

    sidebar collapse button

--- app/ui/shared/nav-links.tsx
+++ app/ui/shared/nav-links.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface NavLinksProps {
   links: Array<{ name: string; href: string }>;
@@ -8,7 +9,8 @@ interface NavLinksProps {
 
 export default function NavLinks({ links }: NavLinksProps) {
   return (
-    <>
+    <div>
+      <ButtonCollapse />
       {links.map((link) => {
         return (
           <Link
@@ -19,6 +21,30 @@ export default function NavLinks({ links }: NavLinksProps) {
           </Link>
         );
       })}
-    </>
+    </div>
+  );
+}
+
+function ButtonCollapse() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    const sidebar = document.querySelector('.sidebar') as HTMLElement | null;
+    if (sidebar) {
+      if (isSidebarOpen) {
+        sidebar.style.width = '50px';
+        sidebar.style.overflow = 'hidden';
+      } else {
+        sidebar.style.width = '200px';
+        sidebar.style.overflow = 'auto';
+      }
+      setIsSidebarOpen(!isSidebarOpen);
+    }
+  };
+
+  return (
+    <button className="buttonRight" onClick={toggleSidebar}>
+      {isSidebarOpen ? '<<' : '>>'}
+    </button>
   );
 }
