b4c8611be275e876804d135cd4603de28cbf22ee
hortar <hrottgers@protonmail.com>
2026-07-16 16:56:27 +0200

    frameworks wip

--- app/ui/shared/sidenav.tsx
+++ app/ui/shared/sidenav.tsx
@@ -1,4 +1,8 @@
-import NavLinks from '@/app/ui/shared/nav-links';
+'use client';
+
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./sidenav.module.css";
 
 interface SideNavProps {
   links: Array<{ name: string; href: string }>;
@@ -9,5 +13,54 @@ export default function SideNav({ links }: SideNavProps) {
     <div>
       <NavLinks links={links} />
     </div>
+  );
+}
+
+interface NavLinksProps {
+  links: Array<{ name: string; href: string }>;
+}
+
+function NavLinks({ links }: NavLinksProps) {
+  return (
+    <div>
+      <div style={{marginBottom: '5px'}}>
+        <ButtonCollapse />
+      </div>
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+          >
+            <p className={styles.sidebarLink}>{link.name}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+function ButtonCollapse() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    const gridLevelTwo = document.querySelector('.gridLevelTwo') as HTMLElement | null;
+    const itemSidebar = document.querySelector('.itemSidebar') as HTMLElement | null;
+    if (gridLevelTwo && itemSidebar) {
+      if (isSidebarOpen) {
+        gridLevelTwo.style.gridTemplate = '1fr / 50px 1fr';
+        itemSidebar.style.overflow = 'hidden';
+      } else {
+        gridLevelTwo.style.gridTemplate = '1fr / 200px 1fr';
+        itemSidebar.style.overflow = 'auto';
+      }
+      setIsSidebarOpen(!isSidebarOpen);
+    }
+  };
+
+  return (
+    <button className={styles.sidebarButton} onClick={toggleSidebar}>
+      {isSidebarOpen ? '<<' : '>>'}
+    </button>
   );
 }
