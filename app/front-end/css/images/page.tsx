68758e45a624aac46d6281542e94cd6d6bbdea80
hortar <hrottgers@protonmail.com>
2026-07-17 22:27:47 +0200

    css:images

--- app/front-end/css/images/page.tsx
+++ app/front-end/css/images/page.tsx
@@ -5,6 +5,8 @@ export default function ImagesPage() {
   return (
     <div>
       <h2>Images</h2>
+      <h3>Image Container</h3>
+      <p>Basic image container using CSS overflow and max-width:</p>
       <SyntaxHighlighter language="html" style={atomDark}>{`<div class="image-container">
         <img src="image.jpg" alt="Image" />
       </div>`}</SyntaxHighlighter>
@@ -17,6 +19,14 @@ export default function ImagesPage() {
         height: auto;
         display: block;
       }`}</SyntaxHighlighter>
+      <h3>Scaling Background Images</h3>
+      <SyntaxHighlighter language="css" style={atomDark}>{`body {
+        background-image: url('#');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+      }`}</SyntaxHighlighter>
+      <p><strong>background-size:</strong> The image will cover the entire background of the element, all while keeping the image in proportion. If the dimensions of the image exceed the dimensions of the container then only a portion of the image will display.</p>
     </div>
   );
 }
