b44246954fc6c1014e64a87482dc3f84c8339089
hortarion <hrottgers@protonmail.com>
2026-07-23 23:19:50 +0200

    dom/manipulation#style

--- app/front-end/dom/manipulation/page.tsx
+++ app/front-end/dom/manipulation/page.tsx
@@ -5,9 +5,21 @@ export default function ManipulationPage() {
   return (
     <div>
       <h1>Manipulation</h1>
+      <ManipulationInnerHTML />
       <ManipulationDocument />
-      <ManipulationDocumentStyle />
+      <ManipulationStyle />
       <br />
+    </div>
+  );
+}
+
+function ManipulationInnerHTML() {
+  return (
+    <div>
+      <h2>InnerHTML</h2>
+      <p>The <code>innerHTML</code> property allows you to change the content of an element.</p>
+      <SyntaxHighlighter language="javascript" style={atomDark}>{`document.body.innerHTML = '<p>New body</p>';`}</SyntaxHighlighter>
+      <p>This replaces the entire content of the <code>body</code> element with a new paragraph.</p>
     </div>
   );
 }
@@ -18,37 +30,41 @@ function ManipulationDocument() {
       <h2>Document</h2>
       <h3>document.querySelector()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.querySelector('p');`}</SyntaxHighlighter>
-      <p><strong>.querySelector</strong> returns the first element that matches the specified selector.</p>
+      <p><code>.querySelector</code> returns the first element that matches the specified selector.</p>
       <h3>document.querySelectorAll()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.querySelectorAll('p');`}</SyntaxHighlighter>
-      <p><strong>.querySelectorAll</strong> returns all elements that match the specified selector.</p>
+      <p><code>.querySelectorAll</code> returns all elements that match the specified selector.</p>
       <br />
       <h3>document.getElementById()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.getElementById('');`}</SyntaxHighlighter>
-      <p><strong>.getElementById</strong> returns the element that has the specified ID.</p>
+      <p><code>.getElementById</code> returns the element that has the specified ID.</p>
       <br />
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.getElementById('div1').innerHTML = '<h1>New Main Heading</h1>';`}</SyntaxHighlighter>
-      <p>Reassign the <strong>innerHTML</strong> property to change the content of the element. In this case, we are replacing the entire content of <strong>div1</strong> with a new <strong>h1</strong> element.</p>
+      <p>Reassign the <code>innerHTML</code> property to change the content of the element. In this case, we are replacing the entire content of <code>div1</code> with a new <code>h1</code> element.</p>
       <h3>document.getElementsByClassName()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.getElementsByClassName('users')[0].innerHTML = '<p>New User</p>';`}</SyntaxHighlighter>
-      <p><strong>.getElementsByClassName</strong> returns <em>all elements</em> that have the specified class name.</p>
+      <p><code>.getElementsByClassName</code> returns <em>all elements</em> that have the specified class name.</p>
       <br />
       <h3>document.getElementsByTagName()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.getElementsByTagName('li')[1].innerHTML = '<p>New Product</p>';`}</SyntaxHighlighter>
-      <p><strong>.getElementsByTagName</strong> returns <em>all elements</em> that have the specified tag name.</p>
+      <p><code>.getElementsByTagName</code> returns <em>all elements</em> that have the specified tag name.</p>
       <br />
       <h3>document.createElement()</h3>
       <SyntaxHighlighter language="javascript" style={atomDark}>{`document.createElement('');`}</SyntaxHighlighter>
-      <p><strong>.createElement</strong> creates a new element with the specified tag name.</p>
+      <p><code>.createElement</code> creates a new element with the specified tag name.</p>
     </div>
   );
 }
 
-function ManipulationDocumentStyle() {
+function ManipulationStyle() {
   return (
     <div>
-      <h2>Document Style</h2>
-      <p>The <strong>style</strong> property allows you to change the style of an element.</p>
+      <h2>Style</h2>
+      <p>The <code>style</code> property allows you to change the style of an element.</p>
+      <SyntaxHighlighter language="javascript" style={atomDark}>{`let firstElement = document.getElementsByClassName('users')[0];
+firstElement.style.backgroundColor = 'red';`}</SyntaxHighlighter>
+      <p>Or chain the property with a selector:</p>
+      <SyntaxHighlighter language="javascript" style={atomDark}>{`document.getElementsByClassName('users')[0].style.backgroundColor = 'red';`}</SyntaxHighlighter>
     </div>
   );
 }
