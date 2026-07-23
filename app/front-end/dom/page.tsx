52ead7eef7a282b055ec0b2818117c4637e19a99
hortarion <hrottgers@protonmail.com>
2026-07-23 21:37:19 +0200

    centered DOM.png

--- app/front-end/dom/page.tsx
+++ app/front-end/dom/page.tsx
@@ -13,12 +13,23 @@ export default function Page() {
         </a>{" "}
         is the Document Object Model, a programming interface for web documents.
       </p>
-      <Image src={"/assets/dom.png"} alt="dom_structure" width={800} height={600} style={{ justifyContent: "center"}}/>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Image
+          src={"/assets/dom.png"}
+          alt="dom_structure"
+          width={800}
+          height={600}
+        />
+      </div>
       <p>
-        It represents the tree-like structure of a web page as a tree of objects, allowing
-        developers to manipulate the page`s content and structure.
+        It represents the tree-like structure of a web page as a tree of
+        objects, allowing developers to manipulate the page`s content and
+        structure.
       </p>
-      <p>It allows for a hierachy within a web page and allows JavaScript to access, modify and update its structure.</p>
+      <p>
+        It allows for a hierachy within a web page and allows JavaScript to
+        access, modify and update its structure.
+      </p>
     </div>
   );
 }
